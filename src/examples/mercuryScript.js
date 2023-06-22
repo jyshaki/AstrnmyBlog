6a53076066a1e7bfbd5a3babb0a891d1bc443113
Patryk.Żuczek <w66002@student.wsiz.edu.pl>
2023-06-22 18:18:59 +0200

    first commit

---
+++ src/examples/mercuryScript.js
@@ -0,0 +1,12 @@
+    import * as THREE from 'three';
+    import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+    import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+    import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+    import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+    import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+
+    let scene, camera, renderer;
+    let model;
+    let mixer;
+    let clock;
+    let controls;
